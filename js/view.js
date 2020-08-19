3e9bb820cc30c0913841272afabd00c8b49059ae
Stig Omdal <stigping@gmail.com>
2020-08-19 14:53:05 +0200

    Modified View

---
+++ js/view.js
@@ -0,0 +1,12 @@
+let appDiv = document.getElementById('app');
+
+function show() {
+appDiv.innerHTML = `
+<div id="grid-container">
+<div id="headerContent">Hei!</div>
+<div id="mainContent">Test</div>
+<div id="footerContent">Test</div>
+</div>
+
+`;
+}
