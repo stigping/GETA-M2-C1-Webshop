6ee3b5e7da670d221304031b9b0616b71dc6a525
Stig Omdal <stigping@gmail.com>
2020-08-19 12:46:32 +0200

    Added Firebase

--- js/model.js
+++ js/model.js
@@ -16,3 +16,31 @@ const model = {
             {username: 'karl', orderList: [], id: '0'}
         ],
 };
+
+function addDatabase() {
+    return db.collection("app").doc('model').set({
+        products: [
+            {id: 0, name: 'Kokkejævelens kyllinggryte', contents: ['Kylling', 'Fløte', 'Grønnsaker'], calories: '247', allergies: 'Melk'},
+            {id: 1, name: 'Alle biffgryters bestemor', contents: ['Biff', 'Grønnsaker'], calories: '237', allergies: ''},
+            {id: 2, name: 'Tom Peng Pong Asiatisk gryte', contents: ['Biff', 'Høns', 'Hund'], calories: '247', allergies: ''},
+            {id: 3, name: 'Lange slappe nudlær', contents: ['Nudler', 'Katt', 'Krydder'], calories: '189', allergies: 'Katt'},
+            {id: 4, name: 'Stor Fin Fisk', contents: ['Fisk', 'Bønner', 'Krydder'], calories: '999', allergies: 'Hai'}
+            ],
+        shoppingBasket: {products: null, amount: null, priceTotal: null},
+        users: [
+            {username: 'jorn', name: 'Jørn', password:'test', id:'0', isAdmin: true},
+            {username: 'karl', name: 'Karl', password:'test', id:'1', isAdmin: false}
+            ],
+        paymentModel: {vipps: {}, paypal: {}, card: {}},
+        orderHistory:[
+            {username: 'karl', orderList: [], id: '0'}
+        ],
+}, { merge:true }
+    
+    ).then(function () {
+        console.log("Document successfully written!");
+    })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
+}
