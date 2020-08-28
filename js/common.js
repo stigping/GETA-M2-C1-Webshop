89bc9d81c53fa720e906950495214dd7d0d558b9
Stig Omdal <stigping@gmail.com>
2020-08-28 13:57:48 +0200

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,26 +1,4 @@
-// Midlertidig kode !!!
-// const selectProduct = () => {
-//     document.getElementById('prodShow').classList.toggle('hidden', false);
-//     const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-//     const body = document.body;
-//     body.style.position = 'fixed';
-//     body.style.top = `-${scrollY}`;
-//   }
-  
-//   const deselectProduct = () => {
-//     const body = document.body;
-//     const scrollY = body.style.top;
-//     body.style.position = '';
-//     body.style.top = '';
-//     window.scrollTo(0, parseInt(scrollY) * -1);
-//     document.getElementById('prodShow').classList.toggle('hidden', true);
-//     window.addEventListener('scroll', () => {
-//     document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
-//     })
-//   }
-
-
-function selectProduct(id, site) {
+function selectProduct(id) {
     model.inputs.productShownId = id;
     if (model.currentPage === 'Home')showHome();
 }
