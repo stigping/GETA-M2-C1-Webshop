458a4035d41371c936e1830a5cd529491036d0b1
Stig Omdal <stigping@gmail.com>
2020-09-16 10:27:36 +0200

    Changes

--- js/homeView.js
+++ js/homeView.js
@@ -1,31 +1,6 @@
 function showHome() {
-    const product = model.products;
-    const productShownId = model.productShownId;
-    const productsLength = model.products.length;
     appDiv.innerHTML = `
-<div  id="prodShow" class="prodShow" ${productShownId != null ? '' : 'style="visibility: hidden"'}>
-    <div  onclick="deselectProduct()" class="prodOverlay"></div>
-    <div id="productBox">
-        <div class="outerImages">
-            <img src="${productsLength <= 0 ? '' : product[productShownId || 0].images[0]}"></img>
-                <div class="slideshow">
-                    <div class="slides">
-                        <img src="${productsLength <= 0 ? '' : product[productShownId || 0].images[1]}">
-                        <img src="${productsLength <= 0 ? '' : product[productShownId || 0].images[2]}">
-                        <img src="${productsLength <= 0 ? '' : product[productShownId || 0].images[3]}">
-                        <img src="${productsLength <= 0 ? '' : product[productShownId || 0].images[4]}">
-                    </div>
-                </div>
-            </div>
-        <div class="outerText">
-            <h3>${productsLength <= 0 ? '' : product[productShownId || 0].name}</h3>
-            <p>${productsLength <= 0 ? '' : product[productShownId || 0].productInfo}</p>
-        </div>
-        <div class="prodButtons">
-            <input type="button" value="Add" onclick="addProduct(${productShownId || 0})">
-        </div>
-    </div>
-</div>
+    ${productShow()}
     <div id="grid-container">
     <div id="headerContent">
         <div class='logoWrap'>
