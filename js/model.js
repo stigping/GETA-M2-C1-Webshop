f3aa0ee6c68e7fa6514b6a6622d55b95b5807915
Stig Omdal <stigping@gmail.com>
2020-09-17 10:57:07 +0200

    Changes

--- js/model.js
+++ js/model.js
@@ -18,7 +18,7 @@ const model = {
             ],
         paymentModel: {vipps: {}, paypal: {}, card: {}},
         orderHistory:[
-            {username: 'jorn', orderList: [{{id: 1, amount: 10, priceTotal: 40}}], id: '0'}
+            {username: 'jorn', orderList: [{id: 1, amount: 10, priceTotal: 40}], id: '0'}
         ],
         productShowBox: {
             productShownId: null,
