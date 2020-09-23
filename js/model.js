542a77f90efd17825dd365632f81edbc88756504
Poablin <kristoffer.elvejord@gmail.com>
2020-09-23 11:58:38 +0200

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,4 +1,5 @@
 const model = {
+    currentPage: 'Home',
     products: [
         { id: 0, name: 'Kokkejævelens kyllinggryte', productInfo: 'Hei jeg er en test setning! Hei jeg er en test setning! Hei jeg er en test setning! Hei jeg er en test setning!', contents: ['Kylling', 'Fløte', 'Grønnsaker'], moms: '0%', calories: '247', price: '50', allergies: 'Melk', stock: '995', purchaseHistory: '7', images: ['https://dbstatic.no/71605105.jpg?imageId=71605105&panow=0&panoh=0&panox=0&panoy=0&heightw=0&heighth=0&heightx=0&heighty=0&width=1200&height=630', 'https://www.pngmart.com/files/1/Grilled-Chicken-Caesar-Salad-PNG.png', 'https://pngriver.com/wp-content/uploads/2018/04/Download-Food-PNG.png', 'https://www.pngmart.com/files/8/Grilled-Food-PNG-Transparent-Image.png', 'https://www.pngkit.com/png/full/112-1129683_shoyu-ramen-ramen-png.png'] },
         { id: 1, name: 'Alle biffgryters bestemor', productInfo: 'Hei jeg er en test setning! Hei jeg er en test setning! Hei jeg er en test setning! Hei jeg er en test setning!', contents: ['Biff', 'Grønnsaker'], moms: '0%', calories: '237', price: '51', allergies: '', stock: '15', purchaseHistory: '10', images: ['https://i2.wp.com/gavehjelp.org/wp-content/uploads/2018/06/gave-til-bestemor.png?resize=1280%2C720&ssl=1', 'https://www.nordichosting.com/u/toff-bestemor.png', 'https://pngriver.com/wp-content/uploads/2018/04/Download-Food-PNG.png', 'https://www.pngmart.com/files/8/Grilled-Food-PNG-Transparent-Image.png', 'https://www.pngkit.com/png/full/112-1129683_shoyu-ramen-ramen-png.png'] },
@@ -7,7 +8,7 @@ const model = {
         { id: 4, name: 'Stor Fin Fisk', productInfo: 'Hei jeg er en test setning! Hei jeg er en test setning! Hei jeg er en test setning! Hei jeg er en test setning!', contents: ['Fisk', 'Bønner', 'Krydder'], moms: '0%', calories: '999', price: '54', allergies: 'Hai', stock: '6', purchaseHistory: '20', images: ['https://www.hooked.no/wp-content/uploads/2017/01/Store-agn-for-stor-fisk-4.jpg', 'https://www.pngmart.com/files/1/Grilled-Chicken-Caesar-Salad-PNG.png', 'https://pngriver.com/wp-content/uploads/2018/04/Download-Food-PNG.png', 'https://www.pngmart.com/files/8/Grilled-Food-PNG-Transparent-Image.png', 'https://www.pngkit.com/png/full/112-1129683_shoyu-ramen-ramen-png.png'] }
     ],
     shoppingBasket: { products: [], priceTotalAll: null },
-    currentUserId: 2,
+    currentUserId: 0,
     users: [
         { id: 0, username: 'jorn', name: 'Jørn', surname: 'Haga', email: 'gmail@mail.no', phoneNumber: 123456789, address: 'Gokrysset 6', password: 'test', isAdmin: true },
         { id: 1, username: 'karl', name: 'Karl', surname: 'Johann', email: 'gmail@mail.no', phoneNumber: 123456789, address: 'Krølveien 4', password: 'test', isAdmin: false },
@@ -48,7 +49,6 @@ const model = {
         currentImage: 0,
         currentTab: 0,
     },
-    currentPage: 'Home',
     inputs: {
         newUserId: 5,
         newProductId: 5,
