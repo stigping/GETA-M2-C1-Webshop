4e04c4b20bce8840a196be8ea3172ff28e12f4d1
Stig Omdal <stigping@gmail.com>
2020-09-28 22:40:19 +0200

    Update loginController.js

--- js/loginController.js
+++ js/loginController.js
@@ -1,14 +1,3 @@
-function login2() {
-    for (i = 0; i < model.users.length; i++) {
-        if (model.users[searchUserIndex(i)].username === model.inputs.user.loginInput.username && model.users[searchUserIndex(i)].password === model.inputs.user.loginInput.password) {
-            model.currentUserId = i;
-            
-        }
-    }
-    showLogin()
-    alert('Brukeren er ikke gjenkjent eller du har skrivd feil!')
-}
-
 function login() {
     let username = '';
     let password = '';
